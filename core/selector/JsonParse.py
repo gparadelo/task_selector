748f2d58119eddf3947343bf96e7d3b9caf4acb8
Gonzalo Paradelo <gonzalo.paradelo@gmail.com>
2022-04-15 16:13:51 -0300

    parsing task list with single quotes

--- core/selector/JsonParse.py
+++ core/selector/JsonParse.py
@@ -6,8 +6,11 @@ from core.selector.Task import Task
 def as_task(dict):
     return Task(dict['name'], set(map(Resource, dict['resources'])), dict['profit'])
 
-def parse_task(json_task):
+def _parse_json_task(json_task):
     return json.loads(json_task, object_hook = as_task)
 
-def parse_task_list(json_task_list):
+def _parse_json_task_list(json_task_list):
     return json.loads(json_task_list, object_hook = as_task)
+
+def parse_task_list(task_list):
+    return _parse_json_task_list(task_list.replace("'", '"'))
--- core/tests/test_json_parse.py
+++ core/tests/test_json_parse.py
@@ -1,24 +1,31 @@
 from django.test import TestCase
-from core.selector.JsonParse import parse_task, parse_task_list
+from core.selector.JsonParse import _parse_json_task, _parse_json_task_list, parse_task_list
 from core.selector.Resource import Resource
 from core.selector.Task import Task
 
 
 class JsonParseTest(TestCase):
 
-    def test_parse_task(self):
+    def test_parse_json_task(self):
         json_task = '{"name": "task1", "resources":["resource1", "resource2"], "profit":2.9}'
         task = Task('task1', {Resource('resource1'), Resource('resource2')}, 2.9)
-        parsed_task = parse_task(json_task)
+        parsed_task = _parse_json_task(json_task)
         self.assertEqual(parsed_task, task)
 
     def test_parse_empty_task_list(self):
         self.assertEqual(parse_task_list('[]'), [])
     
-    def test_parse_task_list(self):
+    def test_parse_json_task_list(self):
         json_task_list = '[{"name": "task1", "resources":["resource1"], "profit":2.9}, ' + \
             '{"name": "task2", "resources":["resource2"], "profit":6}]'
         task1 = Task('task1', {Resource('resource1')}, 2.9)
         task2 = Task('task2', {Resource('resource2')}, 6)
-        self.assertEqual(parse_task_list(json_task_list), [task1, task2])
-        
+        self.assertEqual(_parse_json_task_list(json_task_list), [task1, task2])
+    
+    def test_parse_task_list(self):
+        task_list = "[{'name': 'task1', 'resources':['resource1'], 'profit':2.9}, " + \
+            "{'name': 'task2', 'resources':['resource2'], 'profit':6}]"
+        task1 = Task('task1', {Resource('resource1')}, 2.9)
+        task2 = Task('task2', {Resource('resource2')}, 6)
+        self.assertEqual(parse_task_list(task_list), [task1, task2])
+    
